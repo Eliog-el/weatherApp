31e61664572bfa508f25c6cc9e74ffeffaefd313
Eliog Olusola <elijahogunrinade@gmail.com>
2021-04-22 12:48:18 +0100

    add

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast') 
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 // api.openweathermap.org/data/2.5/weather?lat=9.0820&lon=8.6753&appid=da4546be2a05ba06d06308acee31e16a
 // api.openweathermap.org/data/2.5/weather?q=lagos,Nigeria&units=metric&appid=da4546be2a05ba06d06308acee31e16a
 
-geocode("Boston", (error, data) => {
-    console.log('Error', error);
-    console.log(('Data', data));  
-})
+const address = process.argv[2];
 
-forecast(-75.088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+if (!address) {
+  console.log("please provide an address");
+} else {
+  geocode(address, (error, {longitude, latitude, location }) => {
+    if (error) {
+      return console.log(error); 
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+}
+
+console.log(process.argv);
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,14 +3,13 @@ const request = require("request");
 const forecast = (lat, long, callback) => {
   const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=da4546be2a05ba06d06308acee31e16a`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, response) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
     } else if (response.body.message) {
       callback("Unable to find location", undefined);
     } else {
-      callback(undefined, response.body.weather[0].description + ". It is currently " + response.body.main.temp + " degree out"
-      );
+      callback(undefined, response.body.weather[0].description + ". It is currently " + response.body.main.temp + " degree out");
     }
   });
 };
